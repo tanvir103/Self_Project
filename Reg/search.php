492839047c26d710ae84124e4a1e8c5fd3fb3193
tanvir103 <tanvirh103@gmail.com>
2023-07-29 03:24:09 +0600

    search implemented by ajax

--- Reg/search.php
+++ Reg/search.php
@@ -8,12 +8,40 @@ function search($value){
     $result=mysqli_query($conn,$sql);
     return $result;
 }
-
-    
     $row=search($json);
     if(mysqli_num_rows($row) > 0) {
         while($user = mysqli_fetch_assoc($row)) {
-            echo json_encode($user);
+            $id=$user['id'];
+            $fullname=$user['fullname'];
+            $username=$user['username'];
+            $country=$user['country'];
+            $phone=$user['phonenumber'];
+            $gender=$user['gender'];
+            $email=$user['email'];
+            $dob=$user['dob'];
+            $password=$user['password'];
+            $repass=$user['repassword'];
+            
+            echo"<table>
+            <tr><td>ID: </td>
+            <td>{$id}</td>
+            </tr>
+            <tr><td>Fullname: </td>
+            <td>{$fullname}</td>
+            </tr>
+            <tr><td>Username: </td>
+            <td>{$username}</td>
+            </tr>
+            <tr><td>Country: </td>
+            <td>{$country}</td>
+            </tr>
+            <tr><td>Phone: </td><td>{$phone}</td></tr>
+            <tr><td>Gender:</td><td>{$gender}</td></tr>
+            <tr><td>Email: </td><td>{$email}</td></tr>
+            <tr><td>Date Of Birth: </td><td>{$dob}</td></tr>
+            <tr><td>Password: </td><td>{$password}</td></tr>
+            </table>
+            <br>";
             
         }
     }
