06d8c7c7408e5d95b5385e95ce00b03e6ff8460e
tanvir103 <tanvirh103@gmail.com>
2023-08-04 02:27:14 +0600

    .

--- Reg/dashboard.php
+++ Reg/dashboard.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel='stylesheet' href='style.css'>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
@@ -21,8 +21,7 @@
             <tr><td><a href="edit.php">Edit Profile Information</a></td></tr>
             <tr><td><a href="upload.php">Upload Profile Picture</a></td></tr>
             <tr><td><a href="userinfo.html">View All User</a></td></tr>
-            <tr><td><a href="search.html"><button>Search</button></a></td></tr>
-           
+            <tr><td><a href="search.html"><button style="border: 1 solid #F5C518; background: none; width: 200px;padding: 10px 20px;text-align: center;display: table-cell;vertical-align: middle;font-size: 15px;cursor: pointer;position:relative;overflow: hidden;">Search</button></a></td></tr>
         </tr>
         </table>
     </fieldset>
