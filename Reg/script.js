a3ba2660794513daa7217ed4b06858b6d1e5a547
tanvir103 <tanvirh103@gmail.com>
2023-08-23 03:39:39 +0600

    .

--- Reg/script.js
+++ Reg/script.js
@@ -11,3 +11,24 @@ function search(str){
                 document.getElementById('message').innerHTML=this.responseText;
             }
 }
+function search(str){
+    if(str==""){
+        document.getElementById('message').innerHTML="No User found";
+        return;
+    }
+
+    let username=document.getElementById('live').value;
+   
+    let data=JSON.stringify(username);
+    let xhttp=new XMLHttpRequest();
+    xhttp.open('post','user.php',true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send('name='+data);
+    xhttp.onreadystatechange=function(){
+        if(this.readyState == 4 && this.status == 200){
+            let user = JSON.parse(this.responseText);
+            document.getElementById('message').innerHTML=user.fullname;
+
+        }
+    }
+}
